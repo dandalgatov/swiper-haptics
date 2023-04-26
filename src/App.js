4ce17bef8951d4320d02e9e43efdcc9f981aa516
Dan Dalgatov <dan.dalgatov@gmail.com>
2023-04-25 21:03:17 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import styles from './App.module.css';
+import { useRef, useEffect } from 'react';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 function App() {
+  const swiperElRef = useRef(null);
+
+  const triggerHapticFeedback = () => {
+    if (navigator.vibrate) {
+      console.log('haptic')
+      navigator.vibrate(50);
+    }
+  };
+
+  useEffect(() => {
+    // listen for Swiper events using addEventListener
+    swiperElRef.current.addEventListener('progress', (e) => {
+      const [swiper, progress] = e.detail;
+      console.log(progress);
+      triggerHapticFeedback();
+    });
+
+    swiperElRef.current.addEventListener('slidechange', (e) => {
+      console.log('slide changed');
+      triggerHapticFeedback();
+    });
+  }, []);
+
+  const handleAddToCart = (index) => {
+    console.log(`Add to Cart clicked for slide ${index + 1}`);
+  };
+
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <swiper-container
+          ref={swiperElRef}
+          slides-per-view="3"
+          navigation="true"
+        >       
+          {[...Array(10)].map((_, index) => (
+            <swiper-slide key={index}>
+              <div className={styles.slideContainer}>
+                <img
+                  className={styles.slideImage}
+                  src="https://via.placeholder.com/300"
+                  alt="Product"
+                />
+                <button
+                  className={styles.addButton}
+                  onClick={() => handleAddToCart(index)}
+                >
+                  Add to Cart
+                </button>
+              </div>
+            </swiper-slide>
+          ))}
+        </swiper-container>
       </header>
     </div>
   );
